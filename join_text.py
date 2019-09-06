8b254e12bb4b425e68afc153672513c63003a99f
Willber Nascimento <willberufal@gmail.com>
2019-09-06 17:30:53 -0300

    join_text-dev: add parse arguments --type and --label

--- join_text.py
+++ join_text.py
@@ -1,17 +1,35 @@
 #!/usr/bin/env python3
 
-import os, shutil, time
+import os, time, argparse
+
+# Create the parser
+parser = argparse.ArgumentParser(description='Descreve o programa')
+
+# Add the arguments
+parser.add_argument('--type', action = 'store', dest = 'type',
+                           default = '.txt', required = False,
+                           help = 'O tipo de arquivo que quer concatenar')
+
+parser.add_argument('--label', action = 'store', dest = 'label',
+                           default = "merged", required = False,
+                           help = 'O tipo de arquivo que quer concatenar')
+
+# Execute the parse_args() method
+arguments = parser.parse_args()
+
 
 start_time = time.time()
 
 # name output file
-merged_text = "merged.txt"
+merged_text = arguments.label + arguments.type
 
 # list files to concatenate
 arquivos = []
 
+# search files by type
+
 for file in os.listdir(os.getcwd()):
-    if file.endswith(".txt"):
+    if file.endswith(arguments.type):
         arquivos.append(file)
 
 # open the output file
@@ -26,4 +44,10 @@ for arquivo in arquivos:
 
 merge_file.close()
 
-print("--- Finalized in {:.2f} seconds ---".format(time.time() - start_time))
+total_time = time.time() - start_time
+
+#print("--- Time: {:.2f} seconds ---")
+
+print("Type file: {}".format(arguments.type) + "\n" +
+      "Files concatened: {}".format(len(arquivos)) + "\n" +
+      "Time: {:.2f} seconds". format(total_time))
