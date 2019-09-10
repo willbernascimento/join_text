9e13ecdf4501a82493d231613f0307a7e2734fd6
Willber Nascimento <willberufal@gmail.com>
2019-09-10 06:15:38 -0300

    join_text-dev: working version with better documentation

--- join_text.py
+++ join_text.py
@@ -1,64 +1,71 @@
 #!/usr/bin/env python3
 
-import os, time, argparse, sys
+""" Concatenate multiples text files """
+
+import os
+import time
+import argparse
+import sys
 
 # Create the parser
-parser = argparse.ArgumentParser(description='Descreve o programa')
+parser = argparse.ArgumentParser(description='Program to join multiples text files')
 
 # Add the arguments
-parser.add_argument('--type', action = 'store', dest = 'type',
-                           default = '.txt', required = False,
-                           help = 'O tipo de arquivo que quer concatenar')
+parser.add_argument('--type', action='store', dest='type', default='.txt',
+                    required=False,
+                    help='file type: .txt and .csv is working good')
 
-parser.add_argument('--label', action = 'store', dest = 'label',
-                           default = "merged", required = False,
-                           help = 'O tipo de arquivo que quer concatenar')
+parser.add_argument('--label', action='store', dest='label', default="merged",
+                    required=False, help='The output file name')
 
-parser.add_argument('--override', action = 'store', dest = 'override',
-                           default = "No", required = False,
-                           help = 'O tipo de arquivo que quer concatenar')
+parser.add_argument('--override', action='store', dest='override', default="No",
+                    required=False,
+                    help='Remove existing file with same name as output file')
 
 # Execute the parse_args() method
 arguments = parser.parse_args()
-
 
 start_time = time.time()
 
 # name output file
 merged_text = arguments.label + arguments.type
 
-
+# lists if the file exists and decides what to do
+# if file exist exit program
 if os.path.exists(merged_text) and arguments.override == "No":
     print("Já existe um arquivo com mesmo nome." + "\n" + "Saindo ...")
     sys.exit()
 
+# if file exist: remove it
 if os.path.exists(merged_text) and arguments.override == "Yes":
     print("Overriding existing " + merged_text + " file... \n")
     os.remove(merged_text)
 
 # list files to concatenate
-arquivos = []
+file_list = []
 
 # search files by type
 for file in os.listdir(os.getcwd()):
     if file.endswith(arguments.type):
-        arquivos.append(file)
+        file_list.append(file)
 
 # open the output file
 merge_file = open(merged_text, "wb")
 
-for arquivo in arquivos:
-    text_file = open("./" + arquivo, "rb")
+# write each line of the files in file list
+for file_item in file_list:
+    text_file = open("./" + file_item, "rb")
     for line in text_file:
         merge_file.write(line)
     text_file.close()
 
+# close output file
 merge_file.close()
 
+# get the total time
 total_time = time.time() - start_time
 
-#print("--- Time: {:.2f} seconds ---")
-
+# print information about the precess
 print("Type file: {}".format(arguments.type) + "\n" +
-      "Files concatened: {}".format(len(arquivos)) + "\n" +
-      "Time: {:.2f} seconds". format(total_time))
+      "Files concatened: {}".format(len(file_list)) + "\n" +
+      "Time: {:.2f} seconds".format(total_time))
